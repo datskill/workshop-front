fb78c83f3393bf920d25933acae26e8247483698
hugo thomas <thomas-hugo@orange.fr>
2020-04-14 15:29:54 +0200

    architecture appli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ConnexionComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
